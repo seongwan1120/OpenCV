439ff0ec165b3445ab66156bef9ca0e249727440
SEONGWAN JEONG <125901128+seongwan1120@users.noreply.github.com>
2023-03-31 15:50:06 +0900

    협업 사용자 커밋1

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-import cv2
+from add import add_func  # add.py를 foo가 작성하도록 함. 파라미터 2개.
+from sub import sub_func  # sub.py를 master가 작성. 파라미터 2개.
 
-## 영상 이미지 읽기
+# 전역 변수부
+num1, num2, result = 100, 200, 0
 
-src = cv2.imread('Nature99(Small)/picture01.jpg')
-
-## 영상 처리 알고리즘 구현 ##
-dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
-dst2 = src.copy()
-_, dst2 = cv2.threshold(dst2, 127, 255, cv2.THRESH_BINARY)
-
-
-## 영상 디스플레이
-
-cv2.imshow('src', src)
-cv2.imshow('dst1', dst1)
-cv2.imshow('dst2', dst2)
-
-## 마무리
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+## 메인 코드부
+result = add_func(num1, num2)
+print(result)
+result = sub_func(num1, num2)
+print(result)
